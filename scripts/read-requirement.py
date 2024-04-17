8a8efe7039ff5445fc35eae494e501c1d25f8871
maycuatroi <socrat.nguyenanhbinh@gmail.com>
2024-04-17 09:46:39 +0700

    Bump version to 0.1.12

---
+++ scripts/read-requirement.py
@@ -0,0 +1,32 @@
+import io
+import os
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("evo_science", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+z = read_requirements("../requirements.txt")
+
+print(z)
