ba96c811b6c01e4c6382b6269c23087f199487a6
Nguyễn Anh Bình <sometimesocrazy@gmail.com>
2024-08-26 14:57:57 +0700

    - Update contrastive loss

--- evo_science/dl/losses/contrastive_loss.py
+++ evo_science/dl/losses/contrastive_loss.py
@@ -15,20 +15,18 @@ class ContrastiveLoss(BaseLoss):
         super().__init__()
         self.margin = margin
 
-    def forward(self, output1:torch.Tensor, output2:torch.Tensor, label:torch.Tensor):
+    def forward(self,x1:torch.Tensor,x2:torch.Tensor,y:torch.Tensor):
         """
         Forward pass for contrastive loss.
 
         Args:
-            output1 (torch.Tensor): First output tensor.
-            output2 (torch.Tensor): Second output tensor.
-            label (torch.Tensor): Label tensor.
+            x1 (torch.Tensor): First input tensor.
+            x2 (torch.Tensor): Second input tensor.
+            y (torch.Tensor): Label tensor.
 
         Returns:
             torch.Tensor: Contrastive loss.
         """
-        euclidean_distance = F.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
-                                      label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-        
-        return loss_contrastive
+        dist = F.pairwise_distance(x1, x2)
+        loss = y * torch.pow(dist, 2) + (1 - y) * torch.pow(torch.clamp(self.margin - dist, min=0.0), 2)
+        return torch.mean(loss)
